b78a003067340373066478708609e70e6e330787
jim <northrup.james@gmail.com>
2023-03-27 00:16:54 +0800

    parted out the allocator and the linereader which appear to both work for now

--- src/main/java/ed.fumes/BlockRecycler.kt
+++ src/main/java/ed.fumes/BlockRecycler.kt
@@ -12,20 +12,23 @@ object BlockRecycler {
     val theHeap = ConcurrentSkipListMap<Int, MutableSet<ByteArray>>()
     fun recycleTo(buffer: ByteArray): Unit {
         if (disabled) return
-        val key = (buffer.size + blocksize - 1) / blocksize * blocksize
-        theHeap.getOrPut(key, ::mutableSetOf).add(buffer)
+        val alignedKey = alignedCeiling(buffer.size)
+
+
+        theHeap.getOrPut(alignedKey, ::mutableSetOf).add(buffer)
     }
 
-    fun recycleFrom(available: Int, alignedOnly: Boolean = false): ByteArray {
-        if (disabled) return ByteArray(available)
-        val newkey: Int = (available + blocksize - 1) / blocksize * blocksize
-        return theHeap.tailMap(newkey, true).let { cnm ->
-            cnm.values.firstOrNull { it.isNotEmpty() }?.let {
-                val iterator = it.iterator()
-                val next = iterator.next()
-                iterator.remove()
-                next
-            } ?: ByteArray(if (alignedOnly) newkey else available)
+    fun recycleFrom(minimum: Int, alignedOnly: Boolean = false): ByteArray {
+        if (disabled) return ByteArray(minimum)
+        val alignedKey: Int = alignedCeiling(minimum)
+        return theHeap.tailMap(alignedKey, true).let { cnm ->
+            cnm.values.firstOrNull { it.isNotEmpty() }?.run {
+                iterator().run { next().also { remove() } }
+            } ?: ByteArray(if (alignedOnly) alignedKey else minimum)
         }
+    }
+
+    private fun alignedCeiling(minimum: Int): Int {
+        return (minimum + blocksize - 1) / blocksize * blocksize
     }
 }
